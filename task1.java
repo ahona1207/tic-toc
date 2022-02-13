90a2181f75036ca27d32c2ded010c2349856bb44
ahona1207 <99608066+ahona1207@users.noreply.github.com>
2022-02-13 22:53:53 +0600

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,13 @@
+
+public class task1 {
+	public static void main(String [] args) {
+		double r = 5.5;
+		double area,perimeter;
+		area= 3.1416*5.5*5.5;
+		perimeter=2*3.1416*5.5;
+		System.out.println("area "+area);
+		
+		System.out.println("perimeter "+perimeter);
+	}
+
+}
